610eb2df6bd99220b5e69ca615906338fdd8161c
Nina Zhou <nzhou@lcs.on.ca>
2024-01-29 08:45:57 -0500

    Initial Commit

---
+++ LottieAnimationList/LottieAnimationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationListApp.swift
+//  LottieAnimationList
+//
+//  Created by Shuyu Zhou on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
